db71825ecda6b17997ecbe871272752e262089fb
Rashed Abdullah <maarashed40@gamil.com>
2023-08-29 22:18:15 +0600

    'Async'

--- Async.js
+++ Async.js
@@ -1,1 +1,15 @@
 console.log(`Async Callback`);
+
+function displayfunc(some){
+    console.log(some)
+}
+
+function calculator(number1, number2, callback){
+    let sum = number1 + number2;
+    if(callback) callback(sum);
+    return sum;
+}
+
+calculator(6,6, displayfunc); //12
+calculator(55,6, displayfunc); //61
+calculator(6,6); //12
