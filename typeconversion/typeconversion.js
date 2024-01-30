e8358ac6cf40bd1d17d6a2cdc777370adb73eddb
Rashed Abdullah <maarashed40@gmail.com>
2024-01-30 23:08:27 +0600

    type conversion

---
+++ typeconversion/typeconversion.js
@@ -0,0 +1,21 @@
+// Number to String:
+const numberToString = String(10);
+console.log(typeof numberToString);
+
+const num = 50;
+const numtoStr = num.toString();
+console.log(typeof numtoStr);
+
+//String to Number:
+const stringToNumber = Number("50");
+console.log(typeof stringToNumber);
+
+// String/Number to Boolean:
+const str = "Rashed";
+const empty = "";
+const nums = 5;
+const nums0 = 0;
+console.log(Boolean(str)); //true.
+console.log(Boolean(empty)); //false.
+console.log(Boolean(nums)); //true.
+console.log(Boolean(nums0)); //false.
