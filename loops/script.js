74255d99dab3f0005dc8fde871ea611caa1af181
Rashed Abdullah <maarashed40@gmail.com>
2024-01-31 19:41:00 +0600

    added

--- loops/script.js
+++ loops/script.js
@@ -3,3 +3,28 @@ for (let i = 0; i < 10; i++) {
   sum += i;
 }
 console.log(sum);
+
+const arr = ["Labib", "Ismat", "Mahmud", "Rashed", "Imtiaz", "Itisam"];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+console.log("Break........");
+
+let i = 0;
+
+while (i < arr.length) {
+  console.log(arr[i]);
+  i++;
+}
+
+let winner = true;
+while (winner) {
+  const rang = Math.floor(Math.random() * 10 + 1);
+  if (rang === 5) {
+    console.log("this is winner " + rang);
+    winner = false;
+  } else {
+    console.log(rang);
+  }
+}
