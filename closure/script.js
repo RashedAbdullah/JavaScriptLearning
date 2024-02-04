85d0bfbc0117554617ee3e6574bb34291a333f43
Rashed Abdullah <maarashed40@gmail.com>
2024-02-04 11:58:32 +0600

    closure function practice

--- closure/script.js
+++ closure/script.js
@@ -1,3 +1,6 @@
+// যে ফাংশান ল্যক্সিকাল স্কোপকে মনে রাখতে পারে এবং তার এক্সেস পায়, তাকেই ক্লোজার বলে। এমনকি যখন ঐ ফাংশান যখন ল্যাক্সিকাল স্কােপের বাইরে এক্সিকিউট হয়, তখনও তার এক্সেস পায়।
+
+// যে ফাংশান ল্যক্সিকাল স্কোপকে মনে রাখতে পারে এবং তার এক্সেস পায়, তাকেই ক্লোজার বলে:
 var num = 20;
 function outerFunc() {
   var num2 = 30;
@@ -7,3 +10,13 @@ function outerFunc() {
 }
 var func = outerFunc();
 console.dir(func);
+
+// এমনকি যখন ঐ ফাংশান যখন ল্যাক্সিকাল স্কােপের বাইরে এক্সিকিউট হয়, তখনও তার এক্সেস পায়:
+function closureFunc() {
+  var story = "This is a outer function scope";
+  return function () {
+    return story;
+  };
+}
+var closureVariable = closureFunc();
+console.log(closureVariable()); // This is a outer function scope
