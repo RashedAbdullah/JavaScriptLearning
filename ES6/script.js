e6ad4dd2bafa7e94b07f99e9af66bd77c3260ff0
Rashed Abdullah <maarashed40@gmail.com>
2024-02-06 20:27:22 +0600

    ES6

--- ES6/script.js
+++ ES6/script.js
@@ -1,2 +1,18 @@
 console.log("Rashed".padStart(20, "*"));
 console.log("Rashed".padEnd(20, "*"));
+
+// deafult parameter befor ES6:
+function deafultPara(a, b) {
+  // first way:
+  a = a || 5;
+  // scnd way:
+  if (!b) {
+    b = 10;
+  }
+
+  return a + b;
+}
+console.log(deafultPara());
+
+("بسم الله الرحمن الرحيم");
+console.log("Rashed Abdullah from jamia madania feni bangladesh");
